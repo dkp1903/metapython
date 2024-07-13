6fea9e39aa0ab4a42aa4cfc179a14279dd0ccd2a
Dushyant Pathak <dushyantpathak6789@gmail.com>
2024-07-14 00:10:11 +0530

    Create 1.py

---
+++ mistakes/1.py
@@ -0,0 +1,9 @@
+# While looking through the Localstack codebase, I was able to find extensive use of decorators
+
+# I find these analogous to annotations in Java
+
+# However, in python, the lower your decorator is, the higher priority it's given. The same rule doesn't apply in Java, and can be overridden/customized using @Order.
+
+# This is one place where one needs to be careful
+
+
