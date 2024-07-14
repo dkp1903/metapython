a1d6bf579057a46dc229dba111bcde7163871a1a
Dushyant Pathak <dushyantpathak6789@gmail.com>
2024-07-14 23:04:57 +0530

    Update 1.py

--- mistakes/1.py
+++ mistakes/1.py
@@ -4,7 +4,9 @@
 
 # However, in python, the lower your decorator is, the higher priority it's given. The same rule doesn't apply in Java, and can be overridden/customized using @Order.
 
-# This is one place where one needs to be careful
+# This is one place where one needs to be careful, as decorators are stacked, and not overridden
+
+# In the below example, I might expect that correlID is printed and if not, null is printed, thinking null_adder will be overridden, however, the result being that they're both printed
 
 
 def correl_id(func):
